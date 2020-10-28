a54cb11454020914245b1f9fa6f070288ccd5d73
qishaoxuan <qishaoxuan@jd.com>
2020-10-28 17:04:49 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  useEffect(() => {
+    const { token, userName } = require("./private-config");
+
+    axios
+      .get("https://giki.app/api/users/query?name=qi", {
+        name: userName,
+        headers: { authorization: token }
+      })
+      .then(res => {
+        console.log("res", res.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
+  return <div className="App"></div>;
 }
 
 export default App;
