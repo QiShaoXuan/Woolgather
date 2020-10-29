ce2d8513b4f3a78e8e6964c2d7fffdb67a544143
qishaoxuan <qishaoxuan@jd.com>
2020-10-29 16:00:04 +0800

    update

--- src/private-config.js
+++
@@ -1,3 +0,0 @@
-export const gikiToken =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjMwOGRiMTkyLWNhYjEtNDc5ZS04ZmNjLWY2MmY1OTBlOTFiOSIsIm5hbWUiOiJxaSIsInByb3ZpZGVyIjoiZ2l0aHViIiwicHJvdmlkZXJVc2VySWQiOjIyNzcyODMwLCJpYXQiOjE2MDM4Nzk2MDgsImV4cCI6MTYwNjQ3MTYwOH0.KqU5lUodM4u1VTuEze-aUz7zoxKnjRNkbdGnILQ229g";
-export const userName = "qi";
